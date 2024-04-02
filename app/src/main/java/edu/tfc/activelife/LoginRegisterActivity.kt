44f00ae2c9a1d32057a481a6f1038993c6eee76b
Antonio Luis Garcia <antonlugar777@gmail.com>
2024-04-02 19:29:16 +0200

    AutoLogin

--- app/src/main/java/edu/tfc/activelife/LoginRegisterActivity.kt
+++ app/src/main/java/edu/tfc/activelife/LoginRegisterActivity.kt
@@ -1,11 +1,26 @@
 package edu.tfc.activelife
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginRegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_register_activity)
+        // Verificar si el usuario ya está autenticado
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        if (currentUser != null) {
+            // El usuario ya está autenticado, navegar a la pantalla principal
+            navigateToMain()
+            finish() // Finaliza la actividad actual para que el usuario no pueda volver atrás
+            }
+        }
+
+    private fun navigateToMain() {
+        // Navegar a la pantalla principal de la aplicación
+        startActivity(Intent(this, MainActivity::class.java))
     }
+
 }
