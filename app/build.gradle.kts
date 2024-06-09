2156391f22f348ed8f70789be01ec1f09ecd3944
Antonio Luis Garcia <antonlugar777@gmail.com>
2024-06-09 14:08:05 +0200

    Dokka

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("androidx.navigation.safeargs.kotlin")
     id("com.google.dagger.hilt.android")
     id("org.jetbrains.kotlin.kapt")
+    id("org.jetbrains.dokka")  version "1.9.20"
 }
 
 android {
@@ -40,7 +41,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -53,26 +53,28 @@ dependencies {
     // When using the BoM, you don't specify versions in Firebase library dependencies
     implementation("com.google.firebase:firebase-storage")
 
-    //Fragmentos y navigation
+    // Fragmentos y navigation
     implementation("androidx.fragment:fragment-ktx:1.6.2")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
 
-    //Coil
+    // Coil
     implementation("io.coil-kt:coil:2.5.0")
+    // implementation("io.coil-kt:coil-transformations:2.5.0")
 
-    //Retrofit
+    // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation(("com.squareup.retrofit2:converter-gson:2.9.0"))
 
-    //Glide
-    implementation ("com.github.bumptech.glide:glide:4.14.1")
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.14.1")
 
+    // Room
     implementation("androidx.room:room-ktx:2.6.0")
     implementation("androidx.room:room-runtime:2.6.0")
     kapt("com.github.bumptech.glide:compiler:4.14.1")
 
-    //Firebase
+    // Firebase
     implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth:22.3.1")
@@ -83,15 +85,26 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.48.1")
     kapt("com.google.dagger:hilt-compiler:2.48.1")
 
-
-    //implemnetation("androidx.drawerlayout:drawerlayout:1.2.0")
+    // implementation("androidx.drawerlayout:drawerlayout:1.2.0")
     implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
+
+    //Dokka
+    dokkaPlugin ("org.jetbrains.dokka:android-documentation-plugin:1.9.20")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
 
-
 apply(plugin = "com.google.gms.google-services")
 apply(plugin = "androidx.navigation.safeargs.kotlin")
+apply(plugin="org.jetbrains.dokka")
+
+tasks.dokkaHtml.configure {
+    dokkaSourceSets{
+        configureEach {
+            documentedVisibilities.set(listOf(org.jetbrains.dokka.DokkaConfiguration.Visibility.PRIVATE)
+                    + documentedVisibilities.get())
+            }
+        }
+}
